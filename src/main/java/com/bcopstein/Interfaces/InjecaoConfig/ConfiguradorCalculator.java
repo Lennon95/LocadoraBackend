b433967c786c218872bda260ac790b1b2422152c
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2020-12-01 18:27:39 -0300

    Configura injecao de dependencia dos calculators

---
+++ src/main/java/com/bcopstein/Interfaces/InjecaoConfig/ConfiguradorCalculator.java
@@ -0,0 +1,27 @@
+package com.bcopstein.Interfaces.InjecaoConfig;
+
+import com.bcopstein.Entidades.Dominio.Calculator.SeguroBradesco;
+import com.bcopstein.Entidades.Dominio.Calculator.SeguroCalculator;
+
+import com.bcopstein.Entidades.Dominio.Calculator.Descontos2020;
+import com.bcopstein.Entidades.Dominio.Calculator.DescontoCalculator;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfiguradorCalculator{
+    @Bean
+    @Primary
+    public SeguroCalculator seguroPadrao() {
+        return new SeguroBradesco();
+    }
+ 
+    @Bean
+    @Primary
+    public DescontoCalculator descontoPadrao() {
+        return new Descontos2020();
+    }
+}
