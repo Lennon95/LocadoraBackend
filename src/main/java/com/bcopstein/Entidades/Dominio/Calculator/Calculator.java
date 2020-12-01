4e03fe97add77f923d5427732472940d96b2bee8
Lennon da Silva Rocha <lennon.silva95@gmail.com>
2020-12-01 19:13:55 -0300

    Date para LocalDAte

--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/Calculator.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/Calculator.java
@@ -2,8 +2,8 @@ package com.bcopstein.Entidades.Dominio.Calculator;
 
 import com.bcopstein.Entidades.Dominio.Carro.Carro;
 import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
-import java.util.Date;
+import java.time.LocalDate;
 
 public interface Calculator {
-    public float calcula(Cliente cliente, Carro carro, Date inicio, Date fim);
+    public float calcula(Cliente cliente, Carro carro, LocalDate inicio, LocalDate fim);
 }
--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroPortoSeguro.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroPortoSeguro.java
@@ -2,14 +2,14 @@ package com.bcopstein.Entidades.Dominio.Calculator;
 
 import com.bcopstein.Entidades.Dominio.Carro.Carro;
 import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
-import java.util.Date;
+import java.util.LocalDate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SeguroPortoSeguro implements SeguroCalculator {
 
     @Override
-    public float calcula(Cliente cliente, Carro carro, Date inicio, Date fim) {
+    public float calcula(Cliente cliente, Carro carro, LocalDate inicio, LocalDate fim) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
