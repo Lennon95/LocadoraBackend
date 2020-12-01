4c8e41fe5046d3eea2c3b73932d249a1cf0358ad
Tiago Luz <tiago.piano@gmail.com>
2020-11-30 21:33:36 -0300

    Fix comments

--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroBradesco.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroBradesco.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bcopstein.Entidades.Dominio.Calculator;
 
 import com.bcopstein.Entidades.Dominio.Carro.Carro;
 import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
 import java.util.Date;
 
-/**
- *
- * @author tiagoluz
- */
 public class SeguroBradesco implements SeguroCalculator{
 
     @Override
