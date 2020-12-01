82a9c39cf80ae8b5407d1c0d724f1194b74e68f2
Tiago Luz <tiago.piano@gmail.com>
2020-12-01 20:50:04 -0300

    Fix interfaces

--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/CustoCalculator.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/CustoCalculator.java
@@ -1,5 +1,10 @@
 package com.bcopstein.Entidades.Dominio.Calculator;
 
+import com.bcopstein.Entidades.Dominio.Carro.Carro;
+import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
+import java.time.LocalDate;
+
 public interface CustoCalculator extends Calculator {
-    
+    @Override
+    public float calcula(Cliente cliente, Carro carro, LocalDate inicio, LocalDate fim);
 }
--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroCalculator.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroCalculator.java
@@ -1,5 +1,12 @@
 package com.bcopstein.Entidades.Dominio.Calculator;
 
+import com.bcopstein.Entidades.Dominio.Carro.Carro;
+import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
+import java.time.LocalDate;
+
 public interface SeguroCalculator extends Calculator {
+
+    @Override
+    public float calcula(Cliente cliente, Carro carro, LocalDate inicio, LocalDate fim);
     
 }
