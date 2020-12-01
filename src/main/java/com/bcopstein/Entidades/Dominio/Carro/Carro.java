6ea774d2c509c6ac4c98ab8c32c9a3952793fa23
Tiago Luz <tiago.piano@gmail.com>
2020-11-30 21:09:48 -0300

    Entidades Cor, Carro, Marca, Modelo

--- src/main/java/com/bcopstein/Entidades/Dominio/Carro/Carro.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Carro/Carro.java
@@ -1,49 +1,188 @@
 package com.bcopstein.Entidades.Dominio.Carro;
 
+import java.util.Date;
+
 public class Carro {
     private String placa;
-    private String marca;
-    private String modelo;
-    private boolean arcondicionado;
-    private boolean direcao;
-    private boolean cambioautomatico;
+    private Marca marca;
+    private Modelo modelo;
+    private boolean hasArCondicionado;
+    private boolean hasDirecao;
+    private boolean HasCambioautomatico;
+    
+    private long km;
+    private Cor cor;
+    private boolean isDisponivel;
+    private Date disponivelEm;
+    
 
-    public Carro(String placa, String marca, String modelo, boolean arcondicionado, boolean direcao, boolean cambioautomatico) {
+    public Carro() {
+    }
+    
+    public Carro(String placa, Marca marca, Modelo modelo, boolean hasArCondicionado, boolean hasDirecao, boolean HasCambioautomatico) {
         this.placa = placa;
         this.marca = marca;
         this.modelo = modelo;
-        this.arcondicionado = arcondicionado;
-        this.direcao = direcao;
-        this.cambioautomatico = cambioautomatico;
+        this.hasArCondicionado = hasArCondicionado;
+        this.hasDirecao = hasDirecao;
+        this.HasCambioautomatico = HasCambioautomatico;
     }
 
     public String getPlaca() {
         return placa;
     }
 
-    public String getMarca() {
+    public Marca getMarca() {
         return marca;
     }
 
-    public String getModelo() {
+    public Modelo getModelo() {
         return modelo;
     }
 
-    public boolean isArcondicionado() {
-        return arcondicionado;
+    public boolean hasArCondicionado() {
+        return isHasArCondicionado();
     }
 
-    public boolean isDirecao() {
-        return direcao;
+    public boolean hasDirecao() {
+        return isHasDirecao();
     }
 
-    public boolean isCambioautomatico() {
-        return cambioautomatico;
+    public boolean hasCambioautomatico() {
+        return isHasArCondicionado();
     }
 
     @Override
     public String toString() {
-        return "Carro [arcondicionado=" + arcondicionado + ", cambioautomatico=" + cambioautomatico + ", direcao="
-                + direcao + ", marca=" + marca + ", modelo=" + modelo + ", placa=" + placa + "]";
+        return "Carro [hasArCondicionado=" + isHasArCondicionado() + ", HasCambioautomatico=" + isHasCambioautomatico() + ", hasDirecao="
+                + isHasDirecao() + ", marca=" + getMarca() + ", modelo=" + getModelo() + ", placa=" + getPlaca() + "]";
     } 
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Carro) {
+            return ((Carro) obj).getPlaca().equals(this.getPlaca());
+        }else return false;
+    }
+    
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the hasArCondicionado
+     */
+    public boolean isHasArCondicionado() {
+        return hasArCondicionado;
+    }
+
+    /**
+     * @param hasArCondicionado the hasArCondicionado to set
+     */
+    public void setHasArCondicionado(boolean hasArCondicionado) {
+        this.hasArCondicionado = hasArCondicionado;
+    }
+
+    /**
+     * @return the hasDirecao
+     */
+    public boolean isHasDirecao() {
+        return hasDirecao;
+    }
+
+    /**
+     * @param hasDirecao the hasDirecao to set
+     */
+    public void setHasDirecao(boolean hasDirecao) {
+        this.hasDirecao = hasDirecao;
+    }
+
+    /**
+     * @return the HasCambioautomatico
+     */
+    public boolean isHasCambioautomatico() {
+        return HasCambioautomatico;
+    }
+
+    /**
+     * @param HasCambioautomatico the HasCambioautomatico to set
+     */
+    public void setHasCambioautomatico(boolean HasCambioautomatico) {
+        this.HasCambioautomatico = HasCambioautomatico;
+    }
+
+    /**
+     * @return the km
+     */
+    public long getKm() {
+        return km;
+    }
+
+    /**
+     * @param km the km to set
+     */
+    public void setKm(long km) {
+        this.km = km;
+    }
+
+    /**
+     * @return the cor
+     */
+    public Cor getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(Cor cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the isDisponivel
+     */
+    public boolean isIsDisponivel() {
+        return isDisponivel;
+    }
+
+    /**
+     * @param isDisponivel the isDisponivel to set
+     */
+    public void setIsDisponivel(boolean isDisponivel) {
+        this.isDisponivel = isDisponivel;
+    }
+
+    /**
+     * @return the disponivelEm
+     */
+    public Date getDisponivelEm() {
+        return disponivelEm;
+    }
+
+    /**
+     * @param disponivelEm the disponivelEm to set
+     */
+    public void setDisponivelEm(Date disponivelEm) {
+        this.disponivelEm = disponivelEm;
+    }
+    
+    
 }
---
+++ src/main/java/com/bcopstein/Entidades/Dominio/Carro/Marca.java
@@ -0,0 +1,59 @@
+package com.bcopstein.Entidades.Dominio.Carro;
+
+public class Marca {
+    private String nome;
+    private boolean isLuxo;
+
+    public Marca() {
+    }
+
+    public Marca(String nome, boolean isLuxo) {
+        this.nome = nome;
+        this.isLuxo = isLuxo;
+    }
+
+    @Override
+    public String toString() {
+        return this.nome;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Marca) {
+            return ((Marca) obj).getNome().equals(this.getNome());
+        } else return false;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the isLuxo
+     */
+    public boolean isIsLuxo() {
+        return isLuxo;
+    }
+
+    /**
+     * @param isLuxo the isLuxo to set
+     */
+    public void setIsLuxo(boolean isLuxo) {
+        this.isLuxo = isLuxo;
+    }
+    
+    
+    
+    
+    
+}
