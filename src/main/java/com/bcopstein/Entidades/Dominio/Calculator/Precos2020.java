9f2245a0b1905839864250648d9c0e12b06f1914
Tiago Luz <tiago.piano@gmail.com>
2020-12-01 14:41:13 -0300

    Fix annotations dependency injection

--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/Precos2020.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/Precos2020.java
@@ -3,7 +3,9 @@ package com.bcopstein.Entidades.Dominio.Calculator;
 import com.bcopstein.Entidades.Dominio.Carro.Carro;
 import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
 import java.util.Date;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Precos2020 implements CustoCalculator{
 
     @Override
--- src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroPortoSeguro.java
+++ src/main/java/com/bcopstein/Entidades/Dominio/Calculator/SeguroPortoSeguro.java
@@ -3,7 +3,9 @@ package com.bcopstein.Entidades.Dominio.Calculator;
 import com.bcopstein.Entidades.Dominio.Carro.Carro;
 import com.bcopstein.Entidades.Dominio.Cliente.Cliente;
 import java.util.Date;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SeguroPortoSeguro implements SeguroCalculator {
 
     @Override
